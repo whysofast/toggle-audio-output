a54d97ed31c120496cdb9a01165b2d043566eafa
Matheus Carvalho <39545472+whysofast@users.noreply.github.com>
2023-01-11 10:17:34 -0300

    Update toggle-audio-output.sh

--- toggle-audio-output.sh
+++ toggle-audio-output.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 
-# Obtem as saidas de audio disponiveis
+# Obtem a saida de audio atual
 outputs=$(pactl list sinks | grep "^[[:space:]]Name:[[:space:]]" | cut -d " " -f 2)
 firstOutput=$(echo "$outputs" | sed -n 1p)
 secondOutput=$(echo "$outputs" | sed -n 2p)
 thirdOutput=$(echo "$outputs" | sed -n 3p)
-
-# Obtem a saida de audio atual
 currentOutput=$(pactl info | grep "^Default Sink:" | cut -d " " -f 3-)
-
+index=0
 echo "current output : 
 $currentOutput"
 echo "---------------------------"
-
 echo "available outputs : 
 $outputs"
 echo "---------------------------"
 
+stereoLength=42
+corsairLength=99
+
+if [[ ${#firstOutput} == ${stereoLength} ]]; then
+  stereoOutput="$firstOutput"
+elif [[ ${#secondOutput} == ${stereoLength} ]]; then
+  stereoOutput="$secondOutput"
+elif [[ ${#thirdOutput} == ${stereoLength} ]]; then
+  stereoOutput="$thirdOutput"
+fi
+
+if [[ ${#firstOutput} == ${corsairLength} ]]; then
+  corsairOutput="$firstOutput"
+elif [[ ${#secondOutput} == ${corsairLength} ]]; then
+  corsairOutput="$secondOutput"
+elif [[ ${#thirdOutput} == ${corsairLength} ]]; then
+  corsairOutput="$thirdOutput"
+fi
+
 # Verifica se ha mais de uma saida de audio disponivel
 if [ $(echo "$outputs" | wc -l) -gt 1 ]; then
 
-  if [[ "$currentOutput" == "$secondOutput" ]]; then
-    next="$thirdOutput"
+  if [[ "$currentOutput" == "$stereoOutput" ]]; then
+    next="$corsairOutput"
   else 
-    next="$secondOutput"
+    next="$stereoOutput"
   fi
 
   echo "muda pra $next"
   pactl set-default-sink "$next"
 else
   # Se houver apenas uma saida de audio disponivel, muda para a primeira
-  echo "muda pra $outputs"
+  echo "muda pra primeira"
   pactl set-default-sink "$outputs"
 fi
